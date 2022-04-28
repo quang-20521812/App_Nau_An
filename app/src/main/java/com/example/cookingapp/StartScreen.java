3fccfa138d709a8e11be1e5f33d6ba8443b9b4cf
PHoangLe <lehoangphuc180502@gmail.com>
2022-04-28 20:39:03 +0700

    navigation

--- app/src/main/java/com/example/cookingapp/StartScreen.java
+++ app/src/main/java/com/example/cookingapp/StartScreen.java
@@ -1,17 +1,23 @@
 package com.example.cookingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class StartScreen extends AppCompatActivity {
+
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
-
 
     }
 }
