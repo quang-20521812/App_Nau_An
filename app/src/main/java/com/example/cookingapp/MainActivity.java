f3b9d4c132b1aac747eed53fd8ac2be0f8b09df4
PHoangLe <lehoangphuc180502@gmail.com>
2022-04-28 20:39:03 +0700

    navigation

--- app/src/main/java/com/example/cookingapp/MainActivity.java
+++ app/src/main/java/com/example/cookingapp/MainActivity.java
@@ -1,15 +1,31 @@
 package com.example.cookingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+        setupNavigationView();
+
+    }
+
+    private void setupNavigationView() {
+        navigationView = findViewById(R.id.NavView);
+        NavController navController = Navigation.findNavController(this, R.id.NavHost);
+        NavigationUI.setupWithNavController(navigationView, navController);
     }
 }
