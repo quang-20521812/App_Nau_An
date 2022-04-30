c3c546574733d0e47a52220fa8483e2db9dc05a2
quan <quandang123654@gmail.com>
2022-04-30 22:22:35 +0700

    finish signin, signup

--- app/src/main/java/com/example/cookingapp/SignInScreen.java
+++ app/src/main/java/com/example/cookingapp/SignInScreen.java
@@ -1,24 +1,66 @@
 package com.example.cookingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignInScreen extends AppCompatActivity {
 
+    FirebaseAuth firebaseAuth;
+    EditText editTextEmail,editTextPassword;
+    TextView txtCheckEmail, txtCheckPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_screen);
         getSupportActionBar().hide();
 
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        txtCheckEmail = findViewById(R.id.txtCheckEmail);
+        txtCheckPassword = findViewById(R.id.txtCheckPassword);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        txtCheckEmail.setText("");
+        txtCheckPassword.setText("");
+
         findViewById(R.id.btnSignIn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                startActivity(intent);
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+                if (email.isEmpty()) {
+                    txtCheckEmail.setText("Hãy nhập email!");
+                }
+                if(password.isEmpty()){
+                    txtCheckPassword.setText("Hãy nhập mật khẩu!");
+                }
+                else {
+                    firebaseAuth.signInWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                    });
+                }
             }
         });
         findViewById(R.id.txtSignUp).setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/cookingapp/SignUpScreen.java
+++ app/src/main/java/com/example/cookingapp/SignUpScreen.java
@@ -2,11 +2,20 @@ package com.example.cookingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SignUpScreen extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
+    EditText editTextEmail,editTextPassword,editTextSDT,editTextFullname, editTextConfirmPassWord;
+    TextView txtCheckName, txtCheckMail,txtCheckSDT,txtCheckPassword, txtCheckConfirmPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,19 +23,80 @@ public class SignUpScreen extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up_screen);
         getSupportActionBar().hide();
 
+        editTextEmail = (EditText) findViewById(R.id.editTextEmailSU);
+        editTextPassword = (EditText) findViewById(R.id.editTextPasswordSU);
+        editTextSDT = (EditText) findViewById(R.id.editTextPhoneNumber);
+        editTextFullname = (EditText) findViewById(R.id.editTextName);
+        editTextConfirmPassWord = (EditText) findViewById(R.id.editTextConfirmPass);
+        txtCheckName = findViewById(R.id.txtName);
+        txtCheckSDT = findViewById(R.id.txtCheckSDT);
+        txtCheckMail = findViewById(R.id.txtCheckMail);
+        txtCheckPassword = findViewById(R.id.txtCheckPass);
+        txtCheckConfirmPassword = findViewById(R.id.txtCheckCFPass);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        resetText();
+
         findViewById(R.id.txtSignIn2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                finish();
+                Intent intent = new Intent(getApplicationContext(),SignInScreen.class);
+                startActivity(intent);
             }
         });
 
-        findViewById(R.id.txtSignIn2).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.btnSignUp2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish();
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
+                String confirmPassword = editTextConfirmPassWord.getText().toString();
+                String name = editTextFullname.getText().toString();
+                String sdt = editTextSDT.getText().toString();
+                resetText();
+
+                if (email.isEmpty()) {
+                    txtCheckMail.setText("Hãy nhập địa chỉ email!");
+                }
+                if(password.isEmpty()){
+                    txtCheckPassword.setText("Hãy nhập mật khẩu!");
+                }
+                if(name.isEmpty()){
+                    txtCheckName.setText("Hãy nhập họ và tên!");
+                }
+                if(sdt.isEmpty()){
+                    txtCheckSDT.setText("Hãy nhập số điện thoại!");
+                }
+                if(confirmPassword.isEmpty()){
+                    txtCheckConfirmPassword.setText("Hãy nhập lại mật khẩu!");
+                }
+                if (password.length() < 6) {
+                    txtCheckPassword.setText("Mật khẩu quá ngắn! Yêu cầu mật khẩu từ 6 ký tự trở lên!");
+                }
+                else if (!confirmPassword.equals(password)) {
+                    txtCheckConfirmPassword.setText("Mật khẩu xác nhận chưa đúng!");
+                }
+                else if (confirmPassword.equals(password)) {
+                    firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(SignUpScreen.this, task -> {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Đăng ký thành công !!!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            finish();
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(), "Đăng ký thất bại !!!", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
             }
         });
+    }
+    private void resetText(){
+        txtCheckName.setText("");
+        txtCheckMail.setText("");
+        txtCheckSDT.setText("");
+        txtCheckPassword.setText("");
+        txtCheckConfirmPassword.setText("");
     }
 }
