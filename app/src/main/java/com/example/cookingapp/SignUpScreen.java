432475e1dc0d628bbb0fb3e5e02d4a746c795643
quan <quandang123654@gmail.com>
2022-05-13 10:12:34 +0700

    xong phần đăng nhập, đăng ký, bỏ auth

--- app/src/main/java/com/example/cookingapp/SignUpScreen.java
+++ app/src/main/java/com/example/cookingapp/SignUpScreen.java
@@ -1,5 +1,6 @@
 package com.example.cookingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,32 +10,39 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignUpScreen extends AppCompatActivity {
 
-    FirebaseAuth firebaseAuth;
-    EditText editTextEmail,editTextPassword,editTextSDT,editTextFullname, editTextConfirmPassWord;
-    TextView txtCheckName, txtCheckMail,txtCheckSDT,txtCheckPassword, txtCheckConfirmPassword;
-
+    FirebaseFirestore firestore;
+    EditText editTextUsername,editTextPassword,editTextSDT,editTextFullname, editTextConfirmPassWord;
+    TextView txtCheckName, txtCheckUsername,txtCheckSDT,txtCheckPassword, txtCheckConfirmPassword;
+    Boolean isSuccessSignUp = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_sign_up_screen);
 //        getSupportActionBar().hide();
 
-        editTextEmail = (EditText) findViewById(R.id.editTextEmailSU);
+        editTextUsername = (EditText) findViewById(R.id.editTextUsernameSU);
         editTextPassword = (EditText) findViewById(R.id.editTextPasswordSU);
         editTextSDT = (EditText) findViewById(R.id.editTextPhoneNumber);
         editTextFullname = (EditText) findViewById(R.id.editTextName);
         editTextConfirmPassWord = (EditText) findViewById(R.id.editTextConfirmPass);
         txtCheckName = findViewById(R.id.txtName);
         txtCheckSDT = findViewById(R.id.txtCheckSDT);
-        txtCheckMail = findViewById(R.id.txtCheckMail);
+        txtCheckUsername = findViewById(R.id.txtCheckUsername);
         txtCheckPassword = findViewById(R.id.txtCheckPass);
         txtCheckConfirmPassword = findViewById(R.id.txtCheckCFPass);
 
-        firebaseAuth = FirebaseAuth.getInstance();
+        firestore = FirebaseFirestore.getInstance();
 
         resetText();
 
@@ -49,15 +57,15 @@ public class SignUpScreen extends AppCompatActivity {
         findViewById(R.id.btnSignUp2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String email = editTextEmail.getText().toString();
+                String username = editTextUsername.getText().toString();
                 String password = editTextPassword.getText().toString();
                 String confirmPassword = editTextConfirmPassWord.getText().toString();
                 String name = editTextFullname.getText().toString();
                 String sdt = editTextSDT.getText().toString();
                 resetText();
 
-                if (email.isEmpty()) {
-                    txtCheckMail.setText("Hãy nhập địa chỉ email!");
+                if (username.isEmpty()) {
+                    txtCheckUsername.setText("Hãy nhập tên đăng nhập!");
                 }
                 if(password.isEmpty()){
                     txtCheckPassword.setText("Hãy nhập mật khẩu!");
@@ -78,25 +86,40 @@ public class SignUpScreen extends AppCompatActivity {
                     txtCheckConfirmPassword.setText("Mật khẩu xác nhận chưa đúng!");
                 }
                 else if (confirmPassword.equals(password)) {
-                    firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(SignUpScreen.this, task -> {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(getApplicationContext(), "Đăng ký thành công !!!", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                            finish();
-                        }
-                        else {
-                            Toast.makeText(getApplicationContext(), "Đăng ký thất bại !!!", Toast.LENGTH_SHORT).show();
-                        }
-                    });
+//
+                        pushInfoToFirestore(name,username,password,sdt);
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+//                        finish();
                 }
             }
         });
     }
     private void resetText(){
         txtCheckName.setText("");
-        txtCheckMail.setText("");
+        txtCheckUsername.setText("");
         txtCheckSDT.setText("");
         txtCheckPassword.setText("");
         txtCheckConfirmPassword.setText("");
+    }
+    private void pushInfoToFirestore(String name, String username, String password, String sdt){
+        Map<String,Object> user = new HashMap<>();
+        user.put("Name",name);
+        user.put("Username",username);
+        user.put("Password",password);
+        user.put("Phone",sdt);
+        firestore.collection("User").document(username)
+                .set(user)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(SignUpScreen.this, "Đăng ký thành công!", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(SignUpScreen.this, "Đăng ký thất bại!", Toast.LENGTH_LONG).show();
+                    }
+                });
     }
 }
